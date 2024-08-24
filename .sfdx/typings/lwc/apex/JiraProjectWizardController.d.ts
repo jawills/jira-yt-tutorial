3d9b2a867a47cb7da85ad58f75810328d9943666
Justin Wills <Justin1.wills@gmail.com>
2024-08-24 19:07:32 -0300

    Project wizard

---
+++ .sfdx/typings/lwc/apex/JiraProjectWizardController.d.ts
@@ -0,0 +1,6 @@
+declare module "@salesforce/apex/JiraProjectWizardController.checkJiraKeyAvailable" {
+  export default function checkJiraKeyAvailable(param: {key: any}): Promise<any>;
+}
+declare module "@salesforce/apex/JiraProjectWizardController.createProject" {
+  export default function createProject(param: {key: any, projectName: any, accId: any, userId: any}): Promise<any>;
+}
