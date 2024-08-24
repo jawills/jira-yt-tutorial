d0d5a4ab84cba3eb2af84bc51925d2db6019d9fa
Justin Wills <Justin1.wills@gmail.com>
2024-08-24 17:57:43 -0300

    initial commit

---
+++ .sfdx/typings/lwc/sobjects/DataKitDeploymentLogShare.d.ts
@@ -0,0 +1,44 @@
+declare module "@salesforce/schema/DataKitDeploymentLogShare.Id" {
+  const Id:any;
+  export default Id;
+}
+declare module "@salesforce/schema/DataKitDeploymentLogShare.Parent" {
+  const Parent:any;
+  export default Parent;
+}
+declare module "@salesforce/schema/DataKitDeploymentLogShare.ParentId" {
+  const ParentId:any;
+  export default ParentId;
+}
+declare module "@salesforce/schema/DataKitDeploymentLogShare.UserOrGroup" {
+  const UserOrGroup:any;
+  export default UserOrGroup;
+}
+declare module "@salesforce/schema/DataKitDeploymentLogShare.UserOrGroupId" {
+  const UserOrGroupId:any;
+  export default UserOrGroupId;
+}
+declare module "@salesforce/schema/DataKitDeploymentLogShare.AccessLevel" {
+  const AccessLevel:string;
+  export default AccessLevel;
+}
+declare module "@salesforce/schema/DataKitDeploymentLogShare.RowCause" {
+  const RowCause:string;
+  export default RowCause;
+}
+declare module "@salesforce/schema/DataKitDeploymentLogShare.LastModifiedDate" {
+  const LastModifiedDate:any;
+  export default LastModifiedDate;
+}
+declare module "@salesforce/schema/DataKitDeploymentLogShare.LastModifiedBy" {
+  const LastModifiedBy:any;
+  export default LastModifiedBy;
+}
+declare module "@salesforce/schema/DataKitDeploymentLogShare.LastModifiedById" {
+  const LastModifiedById:any;
+  export default LastModifiedById;
+}
+declare module "@salesforce/schema/DataKitDeploymentLogShare.IsDeleted" {
+  const IsDeleted:boolean;
+  export default IsDeleted;
+}
---
+++ .sfdx/typings/lwc/sobjects/LearningItemShare.d.ts
@@ -0,0 +1,44 @@
+declare module "@salesforce/schema/LearningItemShare.Id" {
+  const Id:any;
+  export default Id;
+}
+declare module "@salesforce/schema/LearningItemShare.Parent" {
+  const Parent:any;
+  export default Parent;
+}
+declare module "@salesforce/schema/LearningItemShare.ParentId" {
+  const ParentId:any;
+  export default ParentId;
+}
+declare module "@salesforce/schema/LearningItemShare.UserOrGroup" {
+  const UserOrGroup:any;
+  export default UserOrGroup;
+}
+declare module "@salesforce/schema/LearningItemShare.UserOrGroupId" {
+  const UserOrGroupId:any;
+  export default UserOrGroupId;
+}
+declare module "@salesforce/schema/LearningItemShare.AccessLevel" {
+  const AccessLevel:string;
+  export default AccessLevel;
+}
+declare module "@salesforce/schema/LearningItemShare.RowCause" {
+  const RowCause:string;
+  export default RowCause;
+}
+declare module "@salesforce/schema/LearningItemShare.LastModifiedDate" {
+  const LastModifiedDate:any;
+  export default LastModifiedDate;
+}
+declare module "@salesforce/schema/LearningItemShare.LastModifiedBy" {
+  const LastModifiedBy:any;
+  export default LastModifiedBy;
+}
+declare module "@salesforce/schema/LearningItemShare.LastModifiedById" {
+  const LastModifiedById:any;
+  export default LastModifiedById;
+}
+declare module "@salesforce/schema/LearningItemShare.IsDeleted" {
+  const IsDeleted:boolean;
+  export default IsDeleted;
+}
